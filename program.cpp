fb44a4bd5c13b63f535a96c6e2df630655fd50a5
lifchrs <68109717+lifchrs@users.noreply.github.com>
2021-04-27 23:37:29 +0800

    Update program.cpp

--- program.cpp
+++ program.cpp
@@ -39,8 +39,6 @@ using vpd = vector<pd>;
 
 #define nn << "\n"
 
-//These macros are from benqi
-
 str yuh, ans;
 
 vector<pair<vs,str> > things;
@@ -48,7 +46,7 @@ vector<pair<vs,str> > things;
 
 void printCards()
 {
-  ofstream cout("1ankiInput.txt");
+  ofstream cout("ankiInput.txt");
   for(pair<vs,str> note : things){
     ans = note.s;
     vs clues = note.f;
@@ -71,19 +69,19 @@ int main()
 {
   cout << "Made by Adhitya Polavaram" nn nn;
   cout <<"Hi there. This is a program that I made which converts notes (it only works for bullet points to the first degree) into flashcards. Each note for a certain topic is created into a flashcard such that the front of the flashcard has the note, and the back of the flashcard has the topic the note described along with all of the other notes you have for the topic in bullet-point form." nn nn nn;
-    cout << "To use this program, first type the answer line then type each of the clues, with each clue being separated by a new line. You can copy paste text into terminal by doing Ctrl + shift + v. Type \"end\" (without the quotation marks) to finish inputting clues for a certain answer. Repeat this for all of your topics. Once you have entered all of the cards that you would like to create, write \"completely done\" (once again, without quotes) for the program to finish. You will see that a file named \"1ankiInput.txt\" will be in the directory that you ran the program in. Each time you use this program, the file will be over-written" nn;
+    cout << "To use this program, first type the answer line then type each of the clues, with each clue being separated by a new line. You can copy paste text into terminal by doing Ctrl + shift + v. Type \"~\" (without the quotation marks) to finish inputting clues for a certain answer. Repeat this for all of your topics. Once you have entered all of the cards that you would like to create, write \"/!\" (once again, without quotes) for the program to finish. You will see that a file named \"ankiInput.txt\" will be in the directory that you ran the program in. Each time you use this program, the file will be over-written" nn;
 
     cout << "Further tip: If your notes are already written in a way such that you have the topic as the first line and ";
     yuh = ans = "";
     bool finishProgram = 0;
     while (!finishProgram) {
-        cout << "Enter an answer" nn;
+        cout << "Enter an answer. If you are done " nn;
         getline(cin, yuh);
-        if (yuh == "completely done")
+        if (yuh == "/!")
             break;
         ans = yuh;
         vs clues;
-        cout << "Enter all of the clues" nn;
+        cout << "Enter all of the clues. If you have finished entering all of the clues for an answer, enter into a new line, and type \"~\"" nn;
         while (1) {
             getline(cin, yuh);
             if(yuh[sz(yuh)-1] == ' '){
@@ -92,9 +90,9 @@ int main()
             if(sz(yuh) == 3){
               for(char& a : yuh) a = tolower(a);
             }
-            if (yuh == "End" || yuh == "end")
+            if (yuh == "~")
                 break;
-            if (yuh == "completely done") {
+            if (yuh == "/!") {
                 finishProgram = 1;
                 break;
             }
